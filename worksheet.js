85c894af79ac8cae94cbb15f03a35bd9a2c8b989
Benjameslane <ben.james.lane9>
2023-07-27 12:50:09 -0600

    Added VS code files to repo

---
+++ worksheet.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//task one
+let dayOfWeek = "Monday";
+console.log(dayOfWeek);
+dayOfWeek = "Friday";
+console.log(`I can't wait for ${dayOfWeek}!`);
+
+//task two
+let animalInput = (prompt("What is your favorite animal?"));
+let colorInput = (prompt("What is your favorite color?"));
+let bothInputs = `${colorInput} ${animalInput}`;
+console.log(`I've never seen a ${bothInputs}!`);
+
+//CONDITIONALS = task one.
