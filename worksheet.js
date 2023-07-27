9493745ea9d3f685d422012b4317de4689fcda07
Benjameslane <ben.james.lane9>
2023-07-27 13:19:59 -0600

    Added conditional portion of assignment

--- worksheet.js
+++ worksheet.js
@@ -12,4 +12,31 @@ let colorInput = (prompt("What is your favorite color?"));
 let bothInputs = `${colorInput} ${animalInput}`;
 console.log(`I've never seen a ${bothInputs}!`);
 
-//CONDITIONALS = task one.
+//CONDITIONALS = task one - may need help on this one -------------------------------------------------
+let timeOfDay = 1300;
+let meal;
+
+if(timeOfDay < 1200){
+    meal = Burrito;
+}
+else if(1700 >= timeOfDay >= 1200){
+    meal = Taco;
+}
+else if(timeOfDay > 1700){
+    meal = Bowl;
+}
+console.log(meal);
+
+//CONDITIONALS = task two 
+let randomNumber = Math.floor(Math.random() * 11);
+if (randomNumber >= 0 && randomNumber <= 2) {
+    console.log("Beatles");
+  } else if (randomNumber >= 3 && randomNumber <= 5) {
+    console.log("Stones");
+  } else if (randomNumber >= 6 && randomNumber <= 8) {
+    console.log("Floyd");
+  } else if (randomNumber === 9 || randomNumber === 10) {
+    console.log("Hendrix");
+  }
+  
+  //FOR LOOPS - task one
